1483643f9d869cdee7531b58979e147a5e0bbe28
mlsoft <mlsoftlaberge@gmail.com>
2016-02-14 10:48:32 -0500

    remove the two-panes layout and code associated with it

--- app/src/main/java/net/ddns/mlsoftlaberge/contactslist/ui/ContactsListActivity.java
+++ app/src/main/java/net/ddns/mlsoftlaberge/contactslist/ui/ContactsListActivity.java
@@ -43,11 +43,6 @@ public class ContactsListActivity extends FragmentActivity implements
     // Defines a tag for identifying log entries
     private static final String TAG = "ContactsListActivity";
 
-    private ContactDetailFragment mContactDetailFragment;
-
-    // If true, this is a larger screen device which fits two panes
-    private boolean isTwoPaneLayout;
-
     // True if this activity instance is a search result view (used on pre-HC devices that load
     // search results in a separate instance of the activity rather than loading results in-line
     // as the query is typed.
@@ -63,10 +58,6 @@ public class ContactsListActivity extends FragmentActivity implements
         // Set main content view. On smaller screen devices this is a single pane view with one
         // fragment. One larger screen devices this is a two pane view with two fragments.
         setContentView(R.layout.activity_main);
-
-        // Check if two pane bool is set based on resource directories
-        isTwoPaneLayout = getResources().getBoolean(R.bool.has_two_panes);
-        // setToast("twopanes = "+isTwoPaneLayout);
 
         // Check if this activity instance has been triggered as a result of a search query. This
         // will only happen on pre-HC OS versions as from HC onward search is carried out using
@@ -91,11 +82,6 @@ public class ContactsListActivity extends FragmentActivity implements
             setTitle(title);
         }
 
-        if (isTwoPaneLayout) {
-            // If two pane layout, locate the contact detail fragment
-            mContactDetailFragment = (ContactDetailFragment)
-                    getSupportFragmentManager().findFragmentById(R.id.contact_detail);
-        }
     }
 
     /**
@@ -106,26 +92,12 @@ public class ContactsListActivity extends FragmentActivity implements
      */
     @Override
     public void onContactSelected(Uri contactUri) {
-        // setToast(contactUri.toString());
-        if (isTwoPaneLayout && mContactDetailFragment != null) {
-            // If two pane layout then update the detail fragment to show the selected contact
-            mContactDetailFragment.setContact(contactUri);
-        } else {
-            // Otherwise single pane layout, start a new ContactDetailActivity with
+            // start a new ContactDetailActivity with
             // the contact Uri
             Intent intent = new Intent(this, ContactDetailActivity.class);
             intent.setData(contactUri);
             startActivity(intent);
-        }
     }
-
-    // Toast Builder
-    // public void setToast(CharSequence text) {
-    //    Context context = getApplicationContext();
-    //    int duration = Toast.LENGTH_LONG;
-    //    Toast toast = Toast.makeText(context, text, duration);
-    //    toast.show();
-    // }
 
     /**
      * This interface callback lets the main contacts list fragment notify
@@ -133,9 +105,7 @@ public class ContactsListActivity extends FragmentActivity implements
      */
     @Override
     public void onSelectionCleared() {
-        if (isTwoPaneLayout && mContactDetailFragment != null) {
-            mContactDetailFragment.setContact(null);
-        }
+
     }
 
     @Override
